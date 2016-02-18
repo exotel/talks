176c6dbdf019a732b0fafb74d2a1160deca750aa
Arpit Mohan <mohanarpit@gmail.com>
2016-02-18 11:08:46 +0530

    Adding minor edits to the sql generator example

---
+++ gophercon-unconference/sql_generator.go
@@ -0,0 +1,70 @@
+// +build OMIT
+
+package main1
+
+import (
+	"bufio"
+	"go/parser"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func main() {
+	err = filepath.Walk(dir, func(path string, f os.FileInfo, err error) error {
+		if !strings.HasSuffix(path, ".sql") {
+			return nil
+		}
+
+		return parser.ParseFile(path, func(raw []string) {
+			invocation, err := parseCommand(trimPath(path), raw)
+			if err != nil {
+				log.Printf("Could not parse command found in %v, %v", path, raw)
+				log.Fatal(err)
+			}
+			invocations = append(invocations, invocation)
+		})
+	})
+}
+
+func (p *Parser) Parse(reader io.Reader, callback Handler) error {
+	var currentCommand []string
+
+	endCommand := func() {
+		if currentCommand != nil {
+			callback(currentCommand)
+		}
+		currentCommand = nil
+	}
+
+	scanner := bufio.NewScanner(reader)
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		if !strings.HasPrefix(line, p.Comment) {
+			endCommand()
+			continue
+		}
+
+		line = strings.TrimSpace(strings.TrimPrefix(line, p.Comment))
+
+		if currentCommand != nil {
+			if line == "" {
+				endCommand()
+				continue
+			}
+
+			currentCommand = append(currentCommand, line)
+			continue
+		}
+
+		if strings.HasPrefix(line, p.Command) {
+			line := strings.TrimSpace(strings.TrimPrefix(line, p.Command))
+			currentCommand = []string{line}
+		}
+	}
+
+	endCommand()
+	return scanner.Err()
+}
